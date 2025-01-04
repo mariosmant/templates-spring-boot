757281972bb40b9b539661c65bc06554a0ec81f3
mariosmant <mariosmant@gmail.com>
2025-01-04 16:25:11 +0200

    CRUD: Initial structure for parent/common/core.

---
+++ crud/core/src/test/java/com/mariosmant/templates/spring/crud/core/CrudCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariosmant.templates.spring.crud.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
